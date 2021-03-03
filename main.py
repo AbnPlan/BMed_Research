871d5817f67f224687ac31d7e1bd0e4557e9537b
Hector A. Medina Torres <hector.medina1997@outlook.com>
2021-03-03 12:50:18 -0400

    Añadi partes del Entry
    Mitad del Entry Form

--- main.py
+++ main.py
@@ -1,44 +1,26 @@
 import kivy
 kivy.require('2.0.0')
 
-from screens import MenuScreen
-from screens import EntryForm
-from screens import Appointments
-from screens import Survey
-from screens import Info
+from screens import *
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, RiseInTransition
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.widget import Widget
-from kivy.uix.button import ButtonBehavior
-from kivy.uix.image import Image
+from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+
+Builder.load_file('main.kv')
+Window.size = (1024, 600)
+Window.borderless = True
 
 class MainApp(App):
     '''Contains the main app and the tittle of the app'''
-    title = 'Med'
     def build(self):
-        '''Builds the app by returning the MainWidget from Kivi file'''
-        sm = ScreenManager()
-        sm.add_widget(MenuScreen(name='menu'))
-        sm.add_widget(EntryForm(name='entry'))
-        sm.add_widget(Appointments(name='appointments'))
-        sm.add_widget(Survey(name='survey'))
-        sm.add_widget(Info(name='info'))
-
-        return sm
-
-class MainButtons(ButtonBehavior, Image):
-    '''Class that makes it possible to have an Image that works like a Button'''
-    def test_button(self, i): 
-        '''Button test function'''
-        print(f"Pressed Button {i}") 
-
+        '''Builds the app by returning the Screen Manager'''
+        return MyScreenManager(transition=RiseInTransition())
 
 if __name__ == "__main__":
-    Builder.load_file('main.kv')
-    Window.size = (1024, 600)
-    Window.borderless = True
     MainApp().run()
--- screens.py
+++ screens.py
@@ -1,22 +1,77 @@
+import csv
+
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
 
-class MenuScreen(Screen, AnchorLayout):
+class MyScreenManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super(MyScreenManager, self).__init__(**kwargs)
+
+    def SMMainScreen(self):
+        '''Change screen to Main Menu'''
+        self.current = 'menu'
+
+    def SMEntry(self):
+        '''Change screen to Entry'''
+        self.current = 'entry'
+
+    def SMAppointments(self):
+        '''Change screen to Appointments'''
+        self.current = 'appointments'
+
+    def SMsurvey(self):
+        '''Change screen to Survey'''
+        self.current = 'survey'
+
+    def SMInfo(self):
+        '''Change screen to Info'''
+        self.current = 'info'
+
+
+class PicButton(ButtonBehavior, Image):
+    '''Class that makes it possible to have an Image that works like a Button'''
+    pass
+
+class MenuScreen(Screen):
     '''Main menu screen'''
     pass
 
-class EntryForm(Screen, AnchorLayout):
+class EntryForm(Screen):
     '''Entry form screen'''
-    pass
+    def clear_text(self):
+        '''Clear all textboxes'''
+        self.ids.full_name.text = ''
+        self.ids.phone_number.text = ''
+        self.ids.address_line1.text = ''
+        self.ids.address_line2.text = ''
+        self.ids.town.text = ''
+        self.ids.country.text = 'Puerto Rico'
 
-class Appointments(Screen, AnchorLayout):
+    def save_form(self):
+        '''Save form'''
+        patient_data = open('DataEntry.csv', 'a')
+        patient_data.write(
+            f'{self.ids.full_name.text},'+
+            f'{self.ids.phone_number.text},'+
+            f'{self.ids.address_line1.text},'+
+            f'{self.ids.address_line2.text},'+
+            f'{self.ids.town.text},'+
+            f'{self.ids.country.text},'+
+            f'{self.ids.zip_code.text}\n'
+            )
+        self.clear_text()
+        patient_data.close()
+
+class Appointments(Screen):
     '''Appointments screen'''
     pass
 
-class Survey(Screen, AnchorLayout):
+class Survey(Screen):
     '''Survey screen'''
     pass
 
-class Info(Screen, AnchorLayout):
+class Info(Screen):
     '''Info screen'''
     pass
